de24df8acccdc5491c1c1f788c1b78bc4e64ac68
onicuto <kiryauvachan@gmail.com>
2026-02-20 22:35:28 +0300

    edit 4 lab

---
+++ Лабораторная 4/probe.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+struct list {
+  int value;
+  list *next;
+  list(int data) : value(data), next(nullptr) {}
+};
+
+void append(list *&head, int value) {
+  list *newlist = new list(value);
+  if (head == nullptr) {
+    head = newlist;
+    return;
+  }
+  list *current = head;
+  while (current->next != nullptr) {
+    current = current->next;
+  }
+  current->next = newlist;
+}
+
+void addlist(list *&head) {
+  int n, b;
+  cout << "Введите количество значений: " << endl;
+  cin >> n;
+  cout << "Введите элементы списка: " << endl;
+  for (int i = 0; i < n; i++) {
+    cin >> b;
+    append(head, b);
+  }
+}
+
+void display(list *head) {
+  if (head == nullptr) {
+    cout << "Список пуст!" << endl;
+  }
+
+  list *current = head;
+  while (current != nullptr) {
+    cout << current->value;
+    if (current->next != nullptr) {
+      cout << " -> ";
+    }
+    current = current->next;
+  }
+  cout << endl;
+}
+
+void deletelist(list *&head) {
+  while (head != nullptr) {
+    list *temp = head;
+    head = head->next;
+    delete temp;
+  }
+}
+
+int main() {
+  list *head = nullptr;
+  addlist(head);
+  display(head);
+}
